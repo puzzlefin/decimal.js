6f15f8d1c43bb10cd59c5e6e192d43437d28ca08
cwikla <cwikla@valenciadata.com>
2021-03-02 10:49:00 -0800

    class X {   private x!: string;   X () {     this.x = "asd";   }   f () {     return this.x;   } } export type T =  keyof X; 10:40 AM T === ‘X’ | ‘f’ (edited) 10:41 AM Remove the private, and you get T === ‘X’  | ‘x’ | ‘f’

--- decimal.d.ts
+++ decimal.d.ts
@@ -56,7 +56,7 @@ export declare class Decimal {
   readonly d: number[];
   readonly e: number;
   readonly s: number;
-  private readonly name: string;
+  readonly name: string;
 
   constructor(n: Decimal.Value);
 
